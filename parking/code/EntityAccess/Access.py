33c9fa50de49c7e7f023bce1f9b8a6380c48e68a
xiaoyang2626 <yanghaocheng_2008@163.com>
2018-06-07 12:16:12 +0800

    完善控制器状态后台

--- parking/code/EntityAccess/Access.py
+++ parking/code/EntityAccess/Access.py
@@ -17,22 +17,24 @@ from EntityAccess.connect import Dao
 class Carin(object):
     def __init__(self):
         self.__CarinDao = Dao()
-    def delect(self,table,pre):
+
+    def delect(self, table, pre):
         try:
-            result=self.__CarinDao.delect(table,pre)
+            result = self.__CarinDao.delect(table, pre)
             return result
         except Exception as ex:
             logging.error(ex)
             return False
-    def select(self, field, requirment,isencode=[]):
-        try:            
+
+    def select(self, field, requirment, isencode=[]):
+        try:
             sql = 'select '
             for i in range(len(field) - 1):
                 sql += field[i] + ','
             sql += field[len(field) - 1]
             sql += requirment
-            logging.debug(sql)            
-            rows = self.__CarinDao.select(sql)            
+            logging.debug(sql)
+            rows = self.__CarinDao.select(sql)
         except Exception as ex:
             logging.error(ex)
             return False
@@ -43,35 +45,39 @@ class Carin(object):
                 for index in range(len(row)):
                     if isinstance(row[index], datetime.datetime):
                         tempdict[field[index]] = row[index].strftime(
-                            "%Y-%m-%d %H:%M:%S")                    
-                    elif field[index] in isencode:                       
-                        tempdict[field[index]] = row[index].encode('latin-1').decode('gbk')
+                            "%Y-%m-%d %H:%M:%S")
+                    elif field[index] in isencode:
+                        tempdict[field[index]] = row[index].encode(
+                            'latin-1').decode('gbk')
                     else:
                         tempdict[field[index]] = str(row[index])
                 # tempdict['id'] = str(number)
                 result.append(tempdict)
             return result
-    def CallProcedurec(self,proc,parameter):
+
+    def CallProcedurec(self, proc, parameter):
         try:
-            rows=self.__CarinDao.procedures(proc,parameter)
-            result=[]
+            rows = self.__CarinDao.procedures(proc, parameter)
+            result = []
             for row in rows:
-                tempdict={'ID':0,'datetime':'','BCar-In':0,'BCar-Out':0,'FreeCardA-In':0,'FreeCardA-Out':0,'Manual-In':0,'Manual-Out':0,'SCar-In':0,'SCar-Out':0,'MRentCardA-In':0,'MRentCardA-出':0,'InCount':0,'OutCount':0}
+                tempdict = {'ID': 0, 'datetime': '', 'BCar-In': 0, 'BCar-Out': 0, 'FreeCardA-In': 0, 'FreeCardA-Out': 0, 'Manual-In': 0,
+                            'Manual-Out': 0, 'SCar-In': 0, 'SCar-Out': 0, 'MRentCardA-In': 0, 'MRentCardA-出': 0, 'InCount': 0, 'OutCount': 0}
                 for index in range(len(row)):
-                    tempdict['ID']=row[0]
-                    tempdict['datetime']=row[1].encode('latin-1').decode('gbk')
-                    tempdict['BCar-In']=row[3]
-                    tempdict['BCar-Out']=row[4]
-                    tempdict['FreeCardA-In']=row[5]
-                    tempdict['FreeCardA-Out']=row[6]
-                    tempdict['Manual-In']=row[7]
-                    tempdict['Manual-Out']=row[8]
-                    tempdict['SCar-In']=row[9]
-                    tempdict['SCar-Out']=row[10]
-                    tempdict['MRentCardA-In']=row[11]
-                    tempdict['MRentCardA-Out']=row[12]
-                    tempdict['InCount']=row[13]
-                    tempdict['OutCount']=row[14]
+                    tempdict['ID'] = row[0]
+                    tempdict['datetime'] = row[1].encode(
+                        'latin-1').decode('gbk')
+                    tempdict['BCar-In'] = row[3]
+                    tempdict['BCar-Out'] = row[4]
+                    tempdict['FreeCardA-In'] = row[5]
+                    tempdict['FreeCardA-Out'] = row[6]
+                    tempdict['Manual-In'] = row[7]
+                    tempdict['Manual-Out'] = row[8]
+                    tempdict['SCar-In'] = row[9]
+                    tempdict['SCar-Out'] = row[10]
+                    tempdict['MRentCardA-In'] = row[11]
+                    tempdict['MRentCardA-Out'] = row[12]
+                    tempdict['InCount'] = row[13]
+                    tempdict['OutCount'] = row[14]
                 result.append(tempdict)
             return result
         except Exception as ex:
--- parking/code/app.py
+++ parking/code/app.py
@@ -4,7 +4,7 @@ monkey.patch_all()
 from gevent.pywsgi import WSGIServer
 
 #gevent end
-import logging;
+import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
@@ -14,165 +14,227 @@ import os
 import EntityAccess.Access
 
 #import flask
-from flask import Flask, request, render_template,redirect,url_for,escape,session,jsonify
-
+from flask import Flask, request, render_template, redirect, url_for, escape, session, jsonify
 
 
 app = Flask(__name__)
-app.config['DEBUG']=True
-app.secret_key=os.urandom(12)
-@app.route('/',methods=['GET'])
+app.config['DEBUG'] = True
+app.secret_key = os.urandom(12)
+
+
+@app.route('/', methods=['GET'])
 def home():
     return render_template('sentrybox.html')
 # 入场信息流水
-@app.route('/GetCarIn',methods=['GET','POST'])
+
+
+@app.route('/GetCarIn', methods=['GET', 'POST'])
 def GetCarIn():
-    st=request.form['starttime']
-    et=request.form['endtime']
-    carinA=EntityAccess.Access.Carin() 
-    if st !='' and et != '':
-        carinlist=carinA.select(['ID','CardID','CarNO','EmpName','InTime','InControlName','InUserName','CardTypeName','InWayName'],' from Vw_Park_CarIn where InTime >= %s and InTime <= %s'%(carinA.gdy(st),carinA.gdy(et)),['InWayName'])
+    st = request.form['starttime']
+    et = request.form['endtime']
+    carinA = EntityAccess.Access.Carin()
+    if st != '' and et != '':
+        carinlist = carinA.select(['ID', 'CardID', 'CarNO', 'EmpName', 'InTime', 'InControlName', 'InUserName', 'CardTypeName', 'InWayName'],
+                                  ' from Vw_Park_CarIn where InTime >= %s and InTime <= %s' % (carinA.gdy(st), carinA.gdy(et)), ['InWayName'])
     else:
-        carinlist=carinA.select(['CardID','CarNO','InTime','InControlName','CardTypeName'],' from Vw_Park_CarIn')
- 
-    
+        carinlist = carinA.select(
+            ['CardID', 'CarNO', 'InTime', 'InControlName', 'CardTypeName'], ' from Vw_Park_CarIn')
+
     if carinlist:
-        result={'data':carinlist}
+        result = {'data': carinlist}
         return jsonify(result)
     else:
-        return jsonify({'type':300,'message':'Not Data'})
-@app.route('/sentrybox',methods=['GET'])
+        return jsonify({'type': 300, 'message': 'Not Data'})
+
+
+@app.route('/sentrybox', methods=['GET'])
 def sentrybox():
     return render_template('sentrybox.html')
 # 出场信息流水
-@app.route('/GetCarOut',methods=['POST'])
+
+
+@app.route('/GetCarOut', methods=['POST'])
 def GetCarOut():
-    caroutA=EntityAccess.Access.Carin()
-    caroutlist=caroutA.select(['InTime','OutTime','OutControlName','CardID','CardType','CarNO'],' from Vw_parK_CarOut')
+    caroutA = EntityAccess.Access.Carin()
+    caroutlist = caroutA.select(
+        ['InTime', 'OutTime', 'OutControlName', 'CardID', 'CardType', 'CarNO'], ' from Vw_parK_CarOut')
     if caroutlist:
-        result={'data':caroutlist}
+        result = {'data': caroutlist}
         return jsonify(result)
     else:
-        return jsonify({'type':300,'message':'Not Data'})
+        return jsonify({'type': 300, 'message': 'Not Data'})
+# 控制器状态
+
+
+@app.route('/GetControl', methods=['GET'])
+def GetControl():
+    control = EntityAccess.Access.Carin()
+    controllist = control.select(
+        ['ContName', 'BoxName', 'DIP', 'RealStatus'], ' from Vw_Park_DeviceStatus')
+    print(controllist)
+    if controllist:
+        templist = []
+        for control in controllist:
+            templist = control['RealStatus'][:7].split(',')            
+            control['Communication']=templist[0]
+            control['Induction']=templist[1]
+            control['Gate']=templist[2]
+            control['CardMachine']=templist[3]
+        return jsonify({'type': 200, 'message': controllist})
+    else:
+        return jsonify({'type': 300, 'message': 'Not Data'})
 # 长期滞留车辆
-@app.route('/GetRetention',methods=['POST'])
+
+
+@app.route('/GetRetention', methods=['POST'])
 def GetRetention():
-    st=request.form['starttime']
-    et=request.form['endtime']
-    carinA=EntityAccess.Access.Carin()
-    carinlist=carinA.select(['ID','CardID','EmpName','CarNO','CardTypeName','CarNoType','InControlName','InTime','InUserName']," from Vw_Park_CarIn where InTime >= %s and InTime <= %s"%(carinA.gdy(st),carinA.gdy(et)))
+    st = request.form['starttime']
+    et = request.form['endtime']
+    carinA = EntityAccess.Access.Carin()
+    carinlist = carinA.select(['ID', 'CardID', 'EmpName', 'CarNO', 'CardTypeName', 'CarNoType', 'InControlName', 'InTime',
+                               'InUserName'], " from Vw_Park_CarIn where InTime >= %s and InTime <= %s" % (carinA.gdy(st), carinA.gdy(et)))
     if carinlist:
-        result={'data':carinlist}
+        result = {'data': carinlist}
         return jsonify(result)
     else:
-        return jsonify({'type':300,'message':'Not Data'})
-@app.route('/DelRetention',methods=['POST'])
+        return jsonify({'type': 300, 'message': 'Not Data'})
+
+
+@app.route('/DelRetention', methods=['POST'])
 def DelRetention():
     try:
-        data=request.get_json()
-        ids=data['ID']
-        carinA=EntityAccess.Access.Carin()
-        pre='where ID in ('
-        for index in range(len(ids)-1):
-            pre+=str(ids[index])+','
-        pre+=str(ids[len(ids)-1])
-        pre+=')'
+        data = request.get_json()
+        ids = data['ID']
+        carinA = EntityAccess.Access.Carin()
+        pre = 'where ID in ('
+        for index in range(len(ids) - 1):
+            pre += str(ids[index]) + ','
+        pre += str(ids[len(ids) - 1])
+        pre += ')'
         logging.debug(pre)
-        carinA.delect('Park_CarIn',pre)
+        carinA.delect('Park_CarIn', pre)
     except Exception as ex:
         logging.error(ex)
-        return jsonify({'type':400,'message':'delect fail'})
+        return jsonify({'type': 400, 'message': 'delect fail'})
     else:
-        return jsonify({'type':200,'message':'delect success'})
-    
-    
-    
+        return jsonify({'type': 200, 'message': 'delect success'})
+
     # result=carinA.delect('Vw_Park_CarIn',pre)
     # if result:
     #     return jsonify({'type':200,'message':'delect success'})
     # else:
     #     return jsonify({'type':400,'message':'delect fail'})
-@app.route('/retention',methods=['GET'])
+
+
+@app.route('/retention', methods=['GET'])
 def Retention():
     return render_template('retention.html')
 # 设备异常登记
-@app.route('/GetAnomaly',methods=['POST'])
+
+
+@app.route('/GetAnomaly', methods=['POST'])
 def GetAnomaly():
-    st=request.form['starttime']
-    et=request.form['endtime']
-    # status=request.form['DeviceStatus'] 
-    carinA=EntityAccess.Access.Carin()
-    carinlist=carinA.select(['ID','DeviceIP','DeviceName','UserDate','DeviceStatus','CreateUserName']," from Park_AllDeviceStatus where  UserDate >= %s and UserDate <= %s"%(carinA.gdy(st),carinA.gdy(et)))
+    st = request.form['starttime']
+    et = request.form['endtime']
+    # status=request.form['DeviceStatus']
+    carinA = EntityAccess.Access.Carin()
+    carinlist = carinA.select(['ID', 'DeviceIP', 'DeviceName', 'UserDate', 'DeviceStatus', 'CreateUserName'],
+                              " from Park_AllDeviceStatus where  UserDate >= %s and UserDate <= %s" % (carinA.gdy(st), carinA.gdy(et)))
     if carinlist:
-        result={'data':carinlist}
+        result = {'data': carinlist}
         return jsonify(result)
     else:
-        return jsonify({'type':300,'message':'Not Data'})
-@app.route('/anomaly',methods=['GET'])
+        return jsonify({'type': 300, 'message': 'Not Data'})
+
+
+@app.route('/anomaly', methods=['GET'])
 def anomaly():
     return render_template('anomaly.html')
 # 场内车辆查询
-@app.route('/vehicleQuery',methods=['GET'])
+
+
+@app.route('/vehicleQuery', methods=['GET'])
 def vehicleQuery():
     return render_template('vehicleQuery.html')
 # 非法开闸查询
-@app.route('/GetIllegality',methods=['POST'])
+
+
+@app.route('/GetIllegality', methods=['POST'])
 def GetIllegality():
-    st=request.form['starttime']
-    et=request.form['endtime'] 
-    carinA=EntityAccess.Access.Carin()
-    carinlist=carinA.select(['ID','CarNO','OutTime','OutControlName','OutUserName','CardTypeName','UnusualMemo']," from Vw_ParK_CarOut where  InTime >= %s and InTime <= %s and CardType < 5 and UnusualMemo !=''"%(carinA.gdy(st),carinA.gdy(et)))
+    st = request.form['starttime']
+    et = request.form['endtime']
+    carinA = EntityAccess.Access.Carin()
+    carinlist = carinA.select(['ID', 'CarNO', 'OutTime', 'OutControlName', 'OutUserName', 'CardTypeName', 'UnusualMemo'],
+                              " from Vw_ParK_CarOut where  InTime >= %s and InTime <= %s and CardType < 5 and UnusualMemo !=''" % (carinA.gdy(st), carinA.gdy(et)))
     if carinlist:
-        result={'data':carinlist}
+        result = {'data': carinlist}
         return jsonify(result)
     else:
-        return jsonify({'type':300,'message':'Not Data'})
-@app.route('/Illegality',methods=['GET'])
+        return jsonify({'type': 300, 'message': 'Not Data'})
+
+
+@app.route('/Illegality', methods=['GET'])
 def Illegality():
     return render_template('illegality.html')
 # 异常出场查询
-@app.route('/GetAbnormal',methods=['POST'])
+
+
+@app.route('/GetAbnormal', methods=['POST'])
 def GetAbnormal():
-    st=request.form['starttime']
-    et=request.form['endtime']
-    # status=request.form['DeviceStatus']    
-    carinA=EntityAccess.Access.Carin()
-    carinlist=carinA.select(['ID','CarNO','EmpName','CardType','CardID','AccountCharge','InTime','InControlName','OutTime','OutControlName','InUserName','OutUserName','OutWayName']," from Vw_ParK_CarOut where  OutTime >= %s and OutTime <= %s and CardType > 7 and OutWay > 0"%(carinA.gdy(st),carinA.gdy(et)),['InUserName','OutWayName'])
+    st = request.form['starttime']
+    et = request.form['endtime']
+    # status=request.form['DeviceStatus']
+    carinA = EntityAccess.Access.Carin()
+    carinlist = carinA.select(['ID', 'CarNO', 'EmpName', 'CardType', 'CardID', 'AccountCharge', 'InTime', 'InControlName', 'OutTime', 'OutControlName', 'InUserName', 'OutUserName',
+                               'OutWayName'], " from Vw_ParK_CarOut where  OutTime >= %s and OutTime <= %s and CardType > 7 and OutWay > 0" % (carinA.gdy(st), carinA.gdy(et)), ['InUserName', 'OutWayName'])
     if carinlist:
-        result={'data':carinlist}
+        result = {'data': carinlist}
         return jsonify(result)
     else:
-        return jsonify({'type':300,'message':'Not Data'})
-@app.route('/abnormal',methods=['GET'])
+        return jsonify({'type': 300, 'message': 'Not Data'})
+
+
+@app.route('/abnormal', methods=['GET'])
 def abnormal():
     return render_template('abnormal.html')
 # 车流量统计
-@app.route('/GetStatistics',methods=['GET','POST'])
+
+
+@app.route('/GetStatistics', methods=['GET', 'POST'])
 def GetStatistics():
-    TableType=request.form['tabletype']  
-    st=request.form['starttime']      
-    carinA=EntityAccess.Access.Carin()
-    carinlist=carinA.CallProcedurec('Sp_Park_CarInOutTrafficHour',(st,TableType))
+    TableType = request.form['tabletype']
+    st = request.form['starttime']
+    carinA = EntityAccess.Access.Carin()
+    carinlist = carinA.CallProcedurec(
+        'Sp_Park_CarInOutTrafficHour', (st, TableType))
     if carinlist:
-        result={'data':carinlist}
+        result = {'data': carinlist}
         return jsonify(result)
     else:
-        return jsonify({'type':300,'message':'Not Data'})
-@app.route('/statistics',methods=['GET'])
+        return jsonify({'type': 300, 'message': 'Not Data'})
+
+
+@app.route('/statistics', methods=['GET'])
 def statistics():
     return render_template('statistics.html')
-@app.route('/ffkz',methods=['POST'])
+
+
+@app.route('/ffkz', methods=['POST'])
 def ffkz():
-    st=request.form['starttime']
-    et=request.form['endtime']
-    return jsonify({'type':200,'message':'success','content':{'starttime':st,'endtime':et}})
-@app.route('/illegality',methods=['GET'])
+    st = request.form['starttime']
+    et = request.form['endtime']
+    return jsonify({'type': 200, 'message': 'success', 'content': {'starttime': st, 'endtime': et}})
+
+
+@app.route('/illegality', methods=['GET'])
 def illegality():
     return render_template('illegality.html')
+
+
 if __name__ == '__main__':
     #app.run(host='0.0.0.0',port=8099)
 
-    http_server=WSGIServer(('0.0.0.0',8099),app)
+    http_server = WSGIServer(('0.0.0.0', 8099), app)
     #logging.debug('服务已启动当前为调试模式')
     # logging.debug('debug message')
     # logging.info('info message')
